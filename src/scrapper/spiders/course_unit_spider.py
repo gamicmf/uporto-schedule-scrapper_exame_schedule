fa388c90c70577b2f55a453cdf354aaedd176f48
Magui <margaridacmf1969@gmail.com>
2026-05-22 06:27:29 +0100

    remove login from course_unit_spider.py

--- src/scrapper/spiders/course_unit_spider.py
+++ src/scrapper/spiders/course_unit_spider.py
@@ -18,8 +18,6 @@ from ..items import CourseUnit
 class CourseUnitSpider(scrapy.Spider):
     name = "course_units"
     allowed_domains = ['sigarra.up.pt']
-    login_page_base = 'https://sigarra.up.pt/feup/pt/mob_val_geral.autentica'
-    password = None
     course_units_ids = set()
 
     def open_config(self):
@@ -33,38 +31,33 @@ class CourseUnitSpider(scrapy.Spider):
     def __init__(self, *args, **kwargs):
         super(CourseUnitSpider, self).__init__(*args, **kwargs)
         self.open_config()
-        self.user = CONFIG[USERNAME]
-        self.password = CONFIG[PASSWORD]
         logging.getLogger('scrapy').propagate = False
 
-    def format_login_url(self):
-        return '{}?{}'.format(self.login_page_base, urlencode({
-            'pv_login': self.user,
-            'pv_password': self.password
-        }))
+    # def format_login_url(self):
+    #     return '{}?{}'.format(self.login_page_base, urlencode({
+    #         'pv_login': self.user,
+    #         'pv_password': self.password
+    #     }))
 
     def start_requests(self):
         "This function is called before crawling starts."
-        if self.password is None:
-            self.password = getpass.getpass(prompt='Password: ', stream=None)
-            
-        yield Request(url=self.format_login_url(), callback=self.check_login_response, errback=self.login_response_err)
-
-    def login_response_err(self, failure):
-        print('Login failed. SIGARRA\'s response: error type 404;\nerror message "{}"'.format(failure))
-        print("Check your password")
+        return self.courseRequests()
     
-    def check_login_response(self, response):
-        """Check the response returned by a login request to see if we are
-        successfully logged in. Since we used the mobile login API endpoint,
-        we can just check the status code.
-        """ 
+    # def login_response_err(self, failure):
+    #     print('Login failed. SIGARRA\'s response: error type 404;\nerror message "{}"'.format(failure))
+    #     print("Check your password")
+    
+    # def check_login_response(self, response):
+    #     """Check the response returned by a login request to see if we are
+    #     successfully logged in. Since we used the mobile login API endpoint,
+    #     we can just check the status code.
+    #     """ 
 
-        if response.status == 200:
-            response_body = json.loads(response.body)
-            if response_body['authenticated']:
-                self.log("Successfully logged in. Let's start crawling!")
-                return self.courseRequests()
+    #     if response.status == 200:
+    #         response_body = json.loads(response.body)
+    #         if response_body['authenticated']:
+    #             self.log("Successfully logged in. Let's start crawling!")
+    #             return self.courseRequests()
            
 
     def courseRequests(self):
@@ -83,49 +76,57 @@ class CourseUnitSpider(scrapy.Spider):
         self.log("Crawling {} courses".format(len(self.courses)))
 
         for course in self.courses:
+            url = 'https://sigarra.up.pt/{}/pt/ucurr_geral.pesquisa_ocorr_ucs_list?pv_ano_lectivo={}&pv_curso_id={}'.format(
+                course[3], course[1], course[2])
+            print(f"[extractSearchPages] URL: {url}")
+            print(f"[courseRequests] Course ID: {course[0]} | Faculty: {course[3]} | Year: {course[1]} | URL: {url}")
             yield scrapy.http.Request(
-                url='https://sigarra.up.pt/{}/pt/ucurr_geral.pesquisa_ocorr_ucs_list?pv_ano_lectivo={}&pv_curso_id={}'.format(
-                    course[3], course[1], course[2]),
+                url=url,
                 meta={'course_id': course[0]},
                 callback=self.extractSearchPages)
 
     def extractSearchPages(self, response):
+        print(f"[extractSearchPages] URL: {response.url} | Status: {response.status}")
         last_page_url = response.css(
             ".paginar-saltar-barra-posicao > div:last-child > a::attr(href)").extract_first()
         last_page = int(parse_qs(urlparse(last_page_url).query)[
             'pv_num_pag'][0]) if last_page_url is not None else 1
+        print(f"[extractSearchPages] Total pages: {last_page}")
         for x in range(1, last_page + 1):
+            page_url = response.url + "&pv_num_pag={}".format(x)
+            print(f"[extractSearchPages] Fetching page {x}: {page_url}")
             yield scrapy.http.Request(
-                url=response.url + "&pv_num_pag={}".format(x),
+                url=page_url,
                 meta=response.meta,
                 callback=self.extractCourseUnits)
 
     def extractCourseUnits(self, response):
+        print(f"[extractCourseUnits] URL: {response.url} | Status: {response.status}")
         course_units_table = response.css("table.dados .d")
+        print(f"[extractCourseUnits] Found {len(course_units_table)} course units")
 
         for course_unit_row in course_units_table:
+            unit_url = response.urljoin(course_unit_row.css(".t > a::attr(href)").extract_first())
+            print(f"[extractCourseUnits] Course unit URL: {unit_url}")
             yield scrapy.http.Request(
-                url=response.urljoin(course_unit_row.css(
-                    ".t > a::attr(href)").extract_first()),
+                url=unit_url,
                 meta=response.meta,
                 callback=self.extractCourseUnitInfo)
 
     def extractCourseUnitInfo(self, response):
+        print(f"[extractCourseUnitInfo] URL: {response.url} | Status: {response.status}")
         name = response.xpath(
             '//div[@id="conteudoinner"]/h1[2]/text()').extract_first().strip()
 
-        # Checks if the course unit page is valid.
-        # If name == 'Sem Resultados', then it is not.
         if name == 'Sem Resultados':
-            return None  # Return None as yielding would continue the program and crash at the next assert
+            print(f"[extractCourseUnitInfo] Sem Resultados for URL: {response.url}")
+            return None
 
-        course_unit_id = parse_qs(urlparse(response.url).query)[
-            'pv_ocorrencia_id'][0]
+        course_unit_id = parse_qs(urlparse(response.url).query)['pv_ocorrencia_id'][0]
 
         acronym = response.xpath(
             '//div[@id="conteudoinner"]/table[@class="formulario"][1]//td[text()="Sigla:"]/following-sibling::td[1]/text()').extract_first()
 
-        # Some pages have Acronym: instead of Sigla:
         if acronym is None: 
             acronym = response.xpath(
                 '//div[@id="conteudoinner"]/table[@class="formulario"][1]//td[text()="Acronym:"]/following-sibling::td[1]/text()').extract_first()
@@ -134,34 +135,20 @@ class CourseUnitSpider(scrapy.Spider):
             acronym = acronym.replace(".", "_")
 
         url = response.url
-        schedule_url = response.xpath(
-            '//a[text()="Horário"]/@href').extract_first()
+        schedule_url = response.xpath('//a[text()="Horário"]/@href').extract_first()
+        print(f"[extractCourseUnitInfo] Name: {name} | Acronym: {acronym} | Schedule URL: {schedule_url}")
 
-        # # If there is no schedule for this course unit
-        # if schedule_url is not None:
-        #     schedule_url = response.urljoin(schedule_url)
-
-        # Occurrence has a string that contains both the year and the semester type
         occurrence = response.css('#conteudoinner > h2::text').extract_first()
-
-        # Possible types: '1', '2', 'A', 'SP'
-        # '1' and '2' represent a semester
-        # 'A' represents an annual course unit
-        # 'SP' represents a course unit without effect this year
         semester = occurrence[24:26].strip()
-
-        # Represents the civil year. If the course unit is taught on the curricular year
-        # 2017/2018, then year is 2017.
         year = int(occurrence[12:16])
+
+        print(f"[extractCourseUnitInfo] Semester: {semester} | Year: {year}")
 
         assert semester == '1S' or semester == '2S' or semester == 'A' or semester == 'SP' \
             or semester == '1T' or semester == '2T' or semester == '3T' or semester == '4T'
-
         assert year > 2000
 
         semesters = []
-
-        # FIXME: Find a better way to allocate trimestral course units
         if semester == '1S' or semester == '1T' or semester == '2T':
             semesters = [1]
         elif semester == '2S' or semester == '3T' or semester == '4T':
@@ -170,8 +157,9 @@ class CourseUnitSpider(scrapy.Spider):
             semesters = [1, 2]
 
         for semester in semesters:
-            if (course_unit_id not in self.course_units_ids) and (schedule_url != None):
+            if (course_unit_id not in self.course_units_ids) and (schedule_url is not None):
                 self.course_units_ids.add(course_unit_id)
+                print(f"[extractCourseUnitInfo] Yielding CourseUnit: {name} (ID: {course_unit_id})")
                 yield CourseUnit(
                     id=course_unit_id,
                     course_id=response.meta['course_id'],
@@ -184,8 +172,7 @@ class CourseUnitSpider(scrapy.Spider):
                     last_updated=datetime.now(),
                 )
             else:
+                print(f"[extractCourseUnitInfo] Skipping CourseUnit: {name} (ID: {course_unit_id}) - already seen or no schedule")
                 yield None
 
-            
-
- 
+    
